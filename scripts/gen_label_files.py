4f6589a8f649ed085dbc46797d599984299934a8
Wanxin Li <w328li@uwaterloo.ca>
2021-03-08 13:52:24 +0800

    debug

--- scripts/gen_label_files.py
+++ scripts/gen_label_files.py
@@ -12,7 +12,7 @@ base_path = "/home/w328li/DeepMicrobes/"
 
 # remove combined_dir if exists
 if os.path.isdir(base_path+combined_dir):
-    os.rmdir(base_path+combined_dir)
+    shutil.rmtree(base_path+combined_dir)
 os.mkdir(base_path+combined_dir)
 
 # concatenate all files in subdir to _combined.fna
@@ -22,12 +22,22 @@ for subdir in os.listdir(base_path+dir):
             with open(filename, 'rb') as readfile:
                 shutil.copyfileobj(readfile, outfile)
 
+# remove label_{dir}.txt if exists
+if os.path.isdir(base_path+label_dir+"label_"+dir+".txt"):
+    shutil.rmtree(base_path+label_dir+"label_"+dir+".txt")
+os.mkdir(base_path+label_dir+"label_"+dir+".txt")
+
 # generate label_{dir}.txt
 label_id = 0
 with open(base_path+label_dir+"label_"+dir+".txt", 'w') as f:
     for fna_file in os.listdir(base_path+combined_dir):
         f.write(fna_file+"\t"+str(label_id)+"\n")
         label_id += 1
+
+# remove name2label_{dir}.txt if exists
+if os.path.isdir(base_path+"data/name2label_"+dir+".txt"):
+    shutil.rmtree(base_path+"data/name2label_"+dir+".txt")
+os.mkdir(base_path+"data/name2label_"+dir+".txt")
 
 # generate name2label_{dir}.txt
 with open(base_path+"data/name2label_"+dir+".txt", "w") as f:
