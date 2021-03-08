7b0d7c40aa84addb78a4683d5d44952ba62504be
Wanxin Li <w328li@uwaterloo.ca>
2021-03-08 14:20:39 +0800

    update

--- scripts/random_trim_all.py
+++ scripts/random_trim_all.py
@@ -6,6 +6,10 @@ base_path = "/home/w328li/DeepMicrobes/"
 label_dir = base_path+dir+"_w_label/"
 trimed_dir = base_path+dir+"_w_label_trimed/"
 
+if os.path.isdir(trimed_dir):
+    shutil.rmtree(trimed_dir)
+os.mkdir(trimed_dir)
+
 # random_trim all files in label_dir
 for file in os.listdir(label_dir):
     input_file = base_path+label_dir+file
