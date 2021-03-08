f128d27881b92a419544d197885952d2fcf8d2b6
Wanxin Li <w328li@uwaterloo.ca>
2021-03-08 14:23:23 +0800

    debug

--- scripts/random_trim_all.py
+++ scripts/random_trim_all.py
@@ -15,6 +15,6 @@ for file in os.listdir(label_dir):
     input_file = base_path+label_dir+file
     output_file = base_path+trimed_dir+"label_trimed"+file[6:]
     os.system("random_trim.py -i " + input_file \
-        "-o " + output_file+ " -f fasta -l 150 -min 0 -max 75")
+        + "-o " + output_file+ " -f fasta -l 150 -min 0 -max 75")
     print("done random_trim.py -i " + input_file \
-        "-o " + output_file+ " -f fasta -l 150 -min 0 -max 75")
+        + "-o " + output_file+ " -f fasta -l 150 -min 0 -max 75")
