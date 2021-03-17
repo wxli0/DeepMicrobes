f2ab7af75fefafe257e2d78fd63d941fbee4a00b
Wanxin Li <w328li@uwaterloo.ca>
2021-03-17 18:02:55 +0800

    debug

--- scripts/extract_first_100_seq.py
+++ scripts/extract_first_100_seq.py
@@ -21,7 +21,7 @@ for file in os.listdir(input_path):
         id, seq = fasta.id, str(fasta.seq)
         out_file.write(">"+id +"\n")
         out_file.write(seq+"\n")
-        out_file.close()
         if counter == 100:
             break
         counter += 1
+    out_file.close()
