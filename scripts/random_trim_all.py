54b0220198f621a62c8f0526cd3472be73d9780c
Wanxin Li <w328li@uwaterloo.ca>
2021-03-18 19:22:23 +0800

    updated random_trim_all.py

--- scripts/random_trim_all.py
+++ scripts/random_trim_all.py
@@ -4,8 +4,8 @@ import shutil
 
 dir = sys.argv[1]
 base_path = "/home/w328li/DeepMicrobes/"
-label_dir = base_path+dir+"_w_label/"
-trimed_dir = base_path+dir+"_w_label_trimed/"
+label_dir = base_path+dir+"/"
+trimed_dir = base_path+dir+"_trimed/"
 
 if os.path.isdir(trimed_dir):
     shutil.rmtree(trimed_dir)
@@ -14,7 +14,7 @@ os.mkdir(trimed_dir)
 # random_trim all files in label_dir
 for file in os.listdir(label_dir):
     input_file = label_dir+file
-    output_file = trimed_dir+"label_trimed_"+prefix
+    output_file = trimed_dir+"trimed_"+file
     os.system("random_trim.py -i " + "'"+ input_file + "'" \
         + " -o " + "'" + output_file+ "'" + " -f fasta -l 150 -min 0 -max 75")
     print("done random_trim.py -i " + "'"+ input_file + "'" \
