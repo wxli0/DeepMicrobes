1418a9d492614f9f25a5b5ddc2929b1ba32d424d
Wanxin Li <w328li@uwaterloo.ca>
2021-04-08 10:05:03 +0800

    debug

--- scripts/result_stat.py
+++ scripts/result_stat.py
@@ -12,6 +12,8 @@ def read_profile(file):
 
 df = pd.read_csv("/home/w328li/MLDSP/samples/Table_S2.csv", skiprows=0, header=1, index_col=0)
 dir = "/mnt/sda/DeepMicrobes-data/mag_reads_250bp_1w_200000_results/"
+precision_list = []
+recall_list = []
 for profile_0 in os.listdir(dir):
     if profile_0.endswith('0_profile.txt'):
         # construct propfile_0_dict
@@ -22,6 +24,7 @@ for profile_0 in os.listdir(dir):
         recall = 0
         if true_label in profile_0_dict:
             recall = profile_0_dict[true_label]/sum(profile_0_dict.values())
+        recall_list.append(recall)
         
         # construct profile_50_dict
         profile_50 = prefix+'.profile.txt'
@@ -29,5 +32,7 @@ for profile_0 in os.listdir(dir):
         precision = 0
         if true_label in profile_50_dict:
             precision = profile_50_dict[true_label]/sum(profile_50_dict.values())
+        precision_list.append(precision)
         print("====", prefix, "true_label:", true_label, "precision:", precision, "recall:", recall)
 
+print("==== avg precision:", sum(precision_list)/len(precision_list), "avg_recall:", sum(recall_list)/len(recall_list))
