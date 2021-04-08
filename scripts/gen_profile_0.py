74d4f40748f071992f14a4f6e0157737dc54aec5
Wanxin Li <w328li@uwaterloo.ca>
2021-04-08 09:33:56 +0800

    added gen_profile_0.py

--- scripts/gen_profile_0.py
+++ scripts/gen_profile_0.py
@@ -15,26 +15,6 @@ for index, row in df.iterrows():
 	result_file = prefix+".result.txt"
 	profile_file = prefix+".profile.txt"
 	profile_0_file = prefix+".0_profile.txt"
-	os.system("tfrec_predict_kmer.sh \
-		-f /mnt/sda/DeepMicrobes-data/mag_reads_250bp_1w_200000/"+forward_file+" \
-		-r /mnt/sda/DeepMicrobes-data/mag_reads_250bp_1w_200000/"+reverse_file+" \
-		-t fasta \
-		-v /mnt/sda/DeepMicrobes-data/tokens_merged_12mers.txt \
-		-o "+prefix+" \
-		-s 4000000 \
-		-k 12")
-	os.system("predict_DeepMicrobes.sh \
-		-i "+tfrec_file+" \
-		-b 8192 \
-		-l genus \
-		-p 8 \
-		-m /mnt/sda/DeepMicrobes-data/weights_genus \
-		-o "+prefix)
-	os.system("report_profile.sh \
-		-i "+result_file+" \
-		-o "+profile_file+" \
-		-t 50 \
-		-l /home/w328li/DeepMicrobes/data/name2label_genus.txt")
 	os.system("report_profile.sh \
 		-i "+result_file+" \
 		-o "+profile_0_file+" \
