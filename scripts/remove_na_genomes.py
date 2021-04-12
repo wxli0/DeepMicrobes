2448be688dae34556137c2ae2a8cb0c089207553
Wanxin Li <w328li@uwaterloo.ca>
2021-04-12 16:43:34 +0800

    debug

--- scripts/remove_na_genomes.py
+++ scripts/remove_na_genomes.py
@@ -7,7 +7,7 @@ import shutil
 
 S1_path = "/Users/wanxinli/Desktop/project.nosync/MLDSP-desktop/samples/Table_S1.csv"
 if platform.node() == 'tml3':
-    S1_path = "~/BlindKameris-new/outputs_HGR/Table_S1.csv"
+    S1_path = "~/MLDSP-desktop/samples/Table_S1.csv"
 S1 = pd.read_csv(S1_path, index_col=0, header=1, dtype = str)
 print(S1['Genus'])
 
