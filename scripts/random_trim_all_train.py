051e7e814c0d73ffb755220a677d92266f1a079a
Wanxin Li <w328li@uwaterloo.ca>
2021-04-14 23:12:21 +0800

    updated radom_trim_all_train.py

--- scripts/random_trim_all_train.py
+++ scripts/random_trim_all_train.py
@@ -2,14 +2,15 @@ import os
 import sys
 import shutil
 
-dir = "/mnt/sda/DeepMicrobes-data/labeled_genome_genus_pruned/"
+dir = "/mnt/sda/DeepMicrobes-data/labeled_genome_genus/"
 
 # random_trim all files in label_dir, assume file ends with .fa
 for file in os.listdir(dir):
-    input_file = dir+file
-    # trim from 3' and 5'
-    output_file = dir+file[:-3]+"_trimmed.fa"
-    os.system("random_trim.py -i " + "'"+ input_file + "'" \
-        + " -o " + "'" + output_file+ "'" + " -f fasta -l 150 -min 0 -max 0 -r False")
-    print("done random_trim.py -i " + "'"+ input_file + "'" \
-        + " -o " + "'" + output_file+ "'" + " -f fasta -l 150 -min 0 -max 0 -r False")
+    if file.endswith('.fa')
+        input_file = dir+file
+        # trim from 3' and 5'
+        output_file = dir+file[:-3]+"_trimmed.fa"
+        os.system("random_trim.py -i " + "'"+ input_file + "'" \
+            + " -o " + "'" + output_file+ "'" + " -f fasta -l 150 -min 0 -max 0 -r False")
+        print("done random_trim.py -i " + "'"+ input_file + "'" \
+            + " -o " + "'" + output_file+ "'" + " -f fasta -l 150 -min 0 -max 0 -r False")
