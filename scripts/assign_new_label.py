63bdbc3e37bb7571341ce94de9ca47859ffeca85
Wanxin Li <w328li@uwaterloo.ca>
2021-04-15 10:09:26 +0800

    debug

--- scripts/assign_new_label.py
+++ scripts/assign_new_label.py
@@ -9,14 +9,11 @@ for file in os.listdir(dir):
         mode = 'w'
         fasta_sequences = SeqIO.parse(open(os.path.join(dir, file)),'fasta')
         for fasta in fasta_sequences:
-            print("enter")
             id, sequence = fasta.id, str(fasta.seq) 
-            print(id)
-            print("> label|"+str(label)+"|"+"|".join(id.split("|", 2)[2:])+"\n")
-            # out_file= open(os.path.join(dir, file[:-3]+'_new.fa'), mode)
-            # out_file.write("> label|"+str(label)+"|"+"|".join(id.split("|", 2)[:2])+"\n")
-            # out_file.write(sequence+"\n")
-            # out_file.close()
-            # mode = 'a'
+            out_file= open(os.path.join(dir, file[:-3]+'_new.fa'), mode)
+            out_file.write("> label|"+str(label)+"|"+"|".join(id.split("|", 2)[2:])+"\n")
+            out_file.write(sequence+"\n")
+            out_file.close()
+            mode = 'a'
         label += 1
         print("done", file)
