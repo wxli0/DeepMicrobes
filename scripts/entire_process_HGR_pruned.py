973d5c098284d2c546415936d548db4c227c0e9e
Wanxin Li <w328li@uwaterloo.ca>
2021-04-15 12:13:47 +0800

    added entire_process_HGR_pruned.py

---
+++ scripts/entire_process_HGR_pruned.py
@@ -0,0 +1,39 @@
+import sys
+import os
+
+dir = "/mnt/sda/DeepMicrobes-data/rumen_mags/"
+for forward_file in os.listdir(dir):
+	if forward_file.endswith('_1.fa'):
+		prefix = forward_file[:-5]
+		print("======================= start", prefix, "=======================")
+		tfrec_file = '/mnt/sda/DeepMicrobes-data/mag_reads_250bp_1w_200000_results/'+prefix+".tfrec"
+		result_file = prefix+".result.txt"
+		profile_file = prefix+".profile.txt"
+		profile_0_file = prefix+".0_profile.txt"
+		category_file = prefix+".category_paired.txt"
+		prob_file = prefix+".prob_paired.txt"
+
+		os.system("DeepMicrobes.py --num_classes=205 \
+			--model_name=attention --encode_method=kmer \
+			--embedding_dim=20 --model_dir=/mnt/sda/DeepMicrobes/HGR_embed_30_weights \
+			--input_tfrec="+tfrec_file + " \
+			--vocab_size=8390658 --cpus=1 \
+			--translate=False --pred_out=" + prefix + " \
+			--running_mode=predict_paired_class")
+		print("======= done DeepMicrobes =======")
+		os.system("paste " + category_file + " " + prob_file + " > " + result_file)
+		os.system("rm " + category_file+ " " + prob_file)
+		os.system("report_profile.sh \
+			-i "+result_file+" \
+			-o "+profile_file+" \
+			-t 50 \
+			-l /home/w328li/DeepMicrobes/data/name2label_gtdb_genus.txt")
+		print("======= done report_profile 50 =======")
+		os.system("report_profile.sh \
+			-i "+result_file+" \
+			-o "+profile_0_file+" \
+			-t 0 \
+			-l /home/w328li/DeepMicrobes/data/name2label_gtdb_genus.txt")
+		print("======= done report_profile_0 =======")
+		print("======================= done", prefix, "=======================")
+		
