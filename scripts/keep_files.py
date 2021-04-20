8722ad94a2690480951af356c13524b89e822f66
Wanxin Li <w328li@uwaterloo.ca>
2021-04-20 18:11:58 +0800

    added keep_files

--- scripts/keep_files.py
+++ scripts/keep_files.py
@@ -11,5 +11,20 @@ for file in os.listdir('/mnt/sda/DeepMicrobes-data/labeled_genome_genus/'):
             frag_id = id.split('|')[4]
             if frag_id in len_dict:
                 print(frag_id)
-            len_dict[frag_id] = len(str(seq))
+            len_dict[frag_id] = len(seq)
+
+
+fasta_sequences = SeqIO.parse(open('/mnt/sda/DeepMicrobes-data/labeled_genome_genus/trimmed_150_var/HGR_train_var.fa', 'fasta'))
+mode = 'w'
+for fasta in fasta_sequences:
+    id, seq = fasta.id, str(fasta.seq)
+    frag_id = id.split('|')[4]
+    if len_dict[frag_id] <= 50000:
+        continue
+
+    out_file= open(os.path.join('/mnt/sda/DeepMicrobes-data/labeled_genome_genus/trimmed_150_var/HGR_pruned_train_var.fa'), mode)
+    out_file.write(">"+id+"\n")
+    out_file.write(seq+"\n")
+    out_file.close()
+    mode = 'a'
 
