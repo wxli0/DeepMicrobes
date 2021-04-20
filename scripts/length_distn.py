5854e24f491fa31641146fb00c1b66ac7a6e42a3
Wanxin Li <w328li@uwaterloo.ca>
2021-04-20 11:38:53 +0800

    debug

--- scripts/length_distn.py
+++ scripts/length_distn.py
@@ -16,6 +16,7 @@ print(lens)
 binwidth = 1000
 bins = np.arange(min(lens), max(lens) + binwidth, binwidth)
 counts, edges, plot = plt.hist(lens, bins=bins, density=True)
-for i in range(len(bins)):
-    print(bins[i], "has", counts[i])
+for i in range(len(bins)-1):
+    if counts[i] != 0:
+        print("["+str(bins[i])+","+str(bins[i+1]), "]:", counts[i])
 plt.savefig("/home/w328li/DeepMicrobes/results/lens_hist.png")
