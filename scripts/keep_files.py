9f789ccb41ef3d237bfc14745a580765cb825f19
Wanxin Li <w328li@uwaterloo.ca>
2021-04-20 17:49:26 +0800

    debug

--- scripts/keep_files.py
+++ scripts/keep_files.py
@@ -8,7 +8,7 @@ for file in os.listdir('/mnt/sda/DeepMicrobes-data/labeled_genome_genus/'):
         fasta_sequences = SeqIO.parse(open("/mnt/sda/DeepMicrobes-data/labeled_genome_genus/"+file),'fasta') 
         for fasta in fasta_sequences:
             id, seq = fasta.id, str(fasta.seq)
-            frag_id = id.split('|')[5]
+            frag_id = id.split('|')[4]
             if frag_id in len_dict:
                 print(frag_id)
             len_dict[frag_id] = len(str(seq))
