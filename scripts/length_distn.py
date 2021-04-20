51ea158f8cf24a3232c9ecd6d501591a6e81a4a2
Wanxin Li <w328li@uwaterloo.ca>
2021-04-20 11:00:13 +0800

    updated length_distn.py

--- scripts/length_distn.py
+++ scripts/length_distn.py
@@ -3,11 +3,13 @@ import sys
 from Bio import SeqIO
 import matplotlib.pyplot as plt
 
-fasta_sequences = SeqIO.parse(open("/mnt/sda/DeepMicrobes-data/labeled_genome_genus/HGR_train.fa"),'fasta') 
 lens = []
-for fasta in fasta_sequences:
-    id, seq = fasta.id, str(fasta.seq)
-    lens.append(len(seq))
+for file in os.listdir('/mnt/sda/DeepMicrobes-data/labeled_genome_genus/'):
+    if file.startswith('label_') and file.endswith('.fa'):
+        fasta_sequences = SeqIO.parse(open("/mnt/sda/DeepMicrobes-data/labeled_genome_genus/"+file),'fasta') 
+        for fasta in fasta_sequences:
+            id, seq = fasta.id, str(fasta.seq)
+            lens.append(len(seq))
 
 plt.hist(lens)
 plt.savefig("/home/w328li/DeepMicrobes/results/lens_hist.png")
