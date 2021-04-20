78602c8749c9d5b9d58d7330046a35a8d0fed9d9
Wanxin Li <w328li@uwaterloo.ca>
2021-04-20 10:45:22 +0800

    added length_distn

---
+++ scripts/length_distn.py
@@ -0,0 +1,13 @@
+import os 
+import sys 
+from Bio import SeqIO
+import matplotlib.pyplot as plt
+
+fasta_sequences = SeqIO.parse(open("/mnt/sda/DeepMicrobes-data/labeled_genome_genus/HGR_train.fa"),'fasta') 
+lens = []
+for fasta in fasta_sequences:
+    id, seq = fasta.id, str(fasta.seq)
+    lens.append(len(seq))
+
+plt.hist(lens)
+plt.savefig("~/DeepMicrobes/results/lens_hist.png")
