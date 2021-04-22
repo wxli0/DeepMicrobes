cc50396f55c0b8cfc7e5fc7df8038d2c288bcda0
Wanxin Li <w328li@uwaterloo.ca>
2021-04-22 16:58:07 +0800

    updated DM_result_stat.py

--- scripts/DM_result_stat.py
+++ scripts/DM_result_stat.py
@@ -11,7 +11,7 @@ def read_profile(file):
 
 
 df = pd.read_csv("/home/w328li/MLDSP/samples/Table_S2.csv", skiprows=0, header=1, index_col=0)
-dir = "/mnt/sda/DeepMicrobes-data/mag_reads_250bp_1w_200000_results/"
+dir = "/mnt/sda/DeepMicrobes-data/HGR_embed_100_results/"
 precision_list = []
 recall_list = []
 for profile_0 in os.listdir(dir):
