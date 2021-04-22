a70d687a3237a6e671219fed4ce764daacfe60bb
Wanxin Li <w328li@uwaterloo.ca>
2021-04-22 11:06:47 +0800

    updated entire_process_HGR_pruned, renamed DM_result_stat.py

--- scripts/entire_process_HGR_pruned.py
+++ scripts/entire_process_HGR_pruned.py
@@ -16,9 +16,9 @@ for index, row in df.iterrows():
 	category_file = prefix+".category_paired.txt"
 	prob_file = prefix+".prob_paired.txt"
 
-	os.system("DeepMicrobes.py --num_classes=120 \
+	os.system("DeepMicrobes.py --num_classes=115 \
 		--model_name=attention --encode_method=kmer \
-		--embedding_dim=30 --model_dir=/mnt/sda/DeepMicrobes/HGR_embed_30_weights \
+		--model_dir=/mnt/sda/DeepMicrobes/HGR_embed_100_weights \
 		--input_tfrec="+tfrec_file + " \
 		--vocab_size=8390658 --cpus=1 \
 		--translate=False --pred_out=" + prefix + " \
@@ -30,13 +30,13 @@ for index, row in df.iterrows():
 		-i "+result_file+" \
 		-o "+profile_file+" \
 		-t 50 \
-		-l /home/w328li/DeepMicrobes/data/name2label_gtdb_genus.txt")
+		-l /home/w328li/DeepMicrobes/data/name2label_pruned_genus.txt")
 	print("======= done report_profile 50 =======")
 	os.system("report_profile.sh \
 		-i "+result_file+" \
 		-o "+profile_0_file+" \
 		-t 0 \
-		-l /home/w328li/DeepMicrobes/data/name2label_gtdb_genus.txt")
+		-l /home/w328li/DeepMicrobes/data/name2label_pruned_genus.txt")
 	print("======= done report_profile_0 =======")
 	print("======================= done", prefix, "=======================")
 		
