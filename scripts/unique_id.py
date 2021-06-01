65a99000a5052c713277c10a9c8c462b260d5cd8
Wanxin Li <w328li@uwaterloo.ca>
2021-06-01 22:52:58 +0800

    debug

--- scripts/unique_id.py
+++ scripts/unique_id.py
@@ -8,12 +8,13 @@ dir = '/mnt/sda/DeepMicrobes-data/mag_reads_250bp_1w_200000_full/'
 for file in os.listdir(dir):
     id = 0
     fasta_sequences = SeqIO.parse(open(dir+file),'fasta') 
+    os.remove(dir+file)
     output_file_path = dir+file
     out_file= open(output_file_path, 'w')
     counter = 0
-    os.remove(dir+file)
     for fasta in fasta_sequences:
         id, seq = fasta.id, str(fasta.seq)
         out_file.write(">"+id +"_"+str(id)+ + "\n")
         out_file.write(seq+"\n")
         id += 1
+        out_file.close()
