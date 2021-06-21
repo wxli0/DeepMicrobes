e2f1beb76bc44672eca6d36c2fafc827918f6480
Wanxin Li <w328li@uwaterloo.ca>
2021-06-21 19:45:49 +0800

    debug

--- scripts/assign_new_label.py
+++ scripts/assign_new_label.py
@@ -5,7 +5,8 @@ from Bio import SeqIO
 label = 0
 dir = '/mnt/sda/MLDSP-samples-r202/dm_species'
 for file in os.listdir(dir):
-    if (file.endswith('.fa') and file.startswith('label_')) or file.endswith('.fna'):
+    if file.endswith('.fa') and file.startswith('label_'):
+        index = file[:-3]
         print("label is:", label)
         mode = 'w'
         fasta_sequences = SeqIO.parse(open(os.path.join(dir, file)),'fasta')
@@ -17,4 +18,17 @@ for file in os.listdir(dir):
             out_file.close()
             mode = 'a'
         label += 1
+    if file.endswith('.fna'):
+        index = file[:-4]
+        print("label is:", label)
+        mode = 'w'
+        fasta_sequences = SeqIO.parse(open(os.path.join(dir, file)),'fasta')
+        for fasta in fasta_sequences:
+            id, sequence = fasta.id, str(fasta.seq) 
+            out_file= open(os.path.join(dir, file[:-3]+'_new.fa'), mode)
+            out_file.write("> label|"+str(label)+"|"+id+"\n")
+            out_file.write(sequence+"\n")
+            out_file.close()
+            mode = 'a'
+        label += 1        
         print("done", file)
