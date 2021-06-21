8255e95646256b2331dc5af4981b77292f3c9596
Wanxin Li <w328li@uwaterloo.ca>
2021-06-21 15:35:55 +0800

    debug

--- scripts/entire_process_HGR_pruned.py
+++ scripts/entire_process_HGR_pruned.py
@@ -4,9 +4,6 @@ import pandas as pd
 
 df = pd.read_csv("/home/w328li/MLDSP/samples/Table_S2.csv", skiprows=0, header=1, index_col=0)
 for index, row in df.iterrows():
-	if row['Genus (reference)'] == 'Unassigned':
-		print("skip", index)
-		continue
 	prefix = "even_"+index
 	print("======================= start", prefix, "=======================")
 	tfrec_file = '/mnt/sda/DeepMicrobes-data/mag_reads_250bp_1w_200000_results/'+prefix+".tfrec"
@@ -16,9 +13,9 @@ for index, row in df.iterrows():
 	category_file = prefix+".category_paired.txt"
 	prob_file = prefix+".prob_paired.txt"
 
-	os.system("DeepMicrobes.py --num_classes=120 \
+	os.system("DeepMicrobes.py --num_classes=2299 \
 		--model_name=attention --encode_method=kmer \
-		--model_dir=/mnt/sda/DeepMicrobes-weights/HGR_var_balanced_embed_100_weights \
+		--model_dir=/mnt/sda/DeepMicrobes-weights/HGR_r202_train_weights \
 		--input_tfrec="+tfrec_file + " \
 		--vocab_size=8390658 --cpus=1 \
 		--translate=False --pred_out=" + prefix + " \
@@ -30,13 +27,13 @@ for index, row in df.iterrows():
 		-i "+result_file+" \
 		-o "+profile_file+" \
 		-t 50 \
-		-l /home/w328li/DeepMicrobes/data/name2label_genus.txt")
+		-l /home/w328li/DeepMicrobes/data/name2label_species_r202.txt")
 	print("======= done report_profile 50 =======")
 	os.system("report_profile.sh \
 		-i "+result_file+" \
 		-o "+profile_0_file+" \
 		-t 0 \
-		-l /home/w328li/DeepMicrobes/data/name2label_genus.txt")
+		-l /home/w328li/DeepMicrobes/data/name2label_species_r202.txt")
 	print("======= done report_profile_0 =======")
 	print("======================= done", prefix, "=======================")
 		
