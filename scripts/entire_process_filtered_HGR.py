a71f4c5de2368ea1bb3d370bf5df7a00335bff2f
Wanxin Li <w328li@uwaterloo.ca>
2021-08-23 12:27:10 +0800

    debug

--- scripts/entire_process_filtered_HGR.py
+++ scripts/entire_process_filtered_HGR.py
@@ -60,11 +60,3 @@ for index, row in df.iterrows():
 		print("======= done report_profile_0 =======")
 		print("======================= done", prefix, "=======================")
 
-
-DeepMicrobes.py --num_classes=2299 \
-			--model_name=attention --encode_method=kmer \
-			--model_dir=/mnt/sda/DeepMicrobes-weights/HGR_r202_train_weights \
-			--input_tfrec="+tfrec_file + " \
-			--vocab_size=8390658 --cpus=1 \
-			--translate=False --pred_out=" + prefix + " \
-			--running_mode=predict_paired_class
