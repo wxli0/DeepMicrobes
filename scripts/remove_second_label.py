69941ba4dc1e361f2b3ed0a63bfaeb6a6b5cfec2
Wanxin Li <w328li@uwaterloo.ca>
2022-03-13 10:25:36 +0800

    debug

--- scripts/remove_second_label.py
+++ scripts/remove_second_label.py
@@ -14,12 +14,17 @@ output_file = "/mnt/sda/DeepMicrobes-data/HGR_species_label_reads/Task1.fa"
 
 fasta_sequences = SeqIO.parse(open(os.path.join(file)),'fasta') 
 out_file= open(output_file, 'w')
+all_labels = []
 for fasta in fasta_sequences:
     id, seq = fasta.id, str(fasta.seq)
     id_split = id.split("|")
     del id_split[2:4]
+    new_label = int(id_split[1])
+    all_labels.append(new_label)
     new_id = "|".join(id_split)
     print(new_id)
     out_file.write(">"+new_id+"\n")
     out_file.write(seq+"\n")
 out_file.close()
+
+print("number of labels is:", len(list(set(all_labels))))
