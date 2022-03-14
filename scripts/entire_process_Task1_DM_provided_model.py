7357a3c86046ecafe9a33ce4955f4e99794e2a58
Wanxin Li <w328li@uwaterloo.ca>
2022-03-14 16:15:39 +0800

    updated

--- scripts/entire_process_Task1_DM_provided_model.py
+++ scripts/entire_process_Task1_DM_provided_model.py
@@ -19,15 +19,18 @@ for index, row in df.iterrows():
 	result_file = prefix+".result.txt"
 	profile_file = prefix+".profile.txt"
 	profile_0_file = prefix+".0_profile.txt"
-	if not os.path.exists(tfrec_file):
-		os.system("tfrec_predict_kmer.sh \
-			-f /mnt/sda/DeepMicrobes-data/mag_reads_150bp_1w_provided_split/"+forward_file+" \
-			-r /mnt/sda/DeepMicrobes-data/mag_reads_150bp_1w_provided_split/"+reverse_file+" \
-			-t fasta \
-			-v /mnt/sda/DeepMicrobes-data/tokens_merged_12mers.txt \
-			-o "+prefix+" \
-			-s 4000000 \
-			-k 12")
+	if not os.path.exists(tfrec_file): # if the tfrecord does not exist in the current dir
+		if os.path.exists(os.path.join(config.DM_data_path, "mag_reads_150bp_1w_split", tfrec_file)): #exists in the other dir, copy
+			os.system("cp "+ os.path.join(config.DM_data_path, "mag_reads_150bp_1w_split", tfrec_file) + " " + tfrec_file)
+		else: # generate
+			os.system("tfrec_predict_kmer.sh \
+				-f /mnt/sda/DeepMicrobes-data/mag_reads_150bp_1w_provided_split/"+forward_file+" \
+				-r /mnt/sda/DeepMicrobes-data/mag_reads_150bp_1w_provided_split/"+reverse_file+" \
+				-t fasta \
+				-v /mnt/sda/DeepMicrobes-data/tokens_merged_12mers.txt \
+				-o "+prefix+" \
+				-s 4000000 \
+				-k 12")
 	# changed batch seize from 8192 to 4096
 	os.system("predict_DeepMicrobes.sh \
 		-i "+tfrec_file+" \
