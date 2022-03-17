a5bc14bddcb7e0f4bc95c9340a701a1822a90a3d
Wanxin Li <w328li@uwaterloo.ca>
2022-03-17 01:49:56 -0400

    added label mappping files

---
+++ scripts/auto_art_Task3_1.sh
@@ -0,0 +1,4 @@
+art_illumina --noALN -ss HS25 -i 'label_s__Methanobrevibacter_B wolinii.fa' -p -o 'art_s__Methanobrevibacter_B wolinii.' -l 150 -f 1.4692817269349705 -m 200 -s 10 -rs 1
+art_illumina --noALN -ss HS25 -i 'label_s__Methanobrevibacter_B sp002208625.fa' -p -o 'art_s__Methanobrevibacter_B sp002208625.' -l 150 -f 1.5619516898755332 -m 200 -s 10 -rs 1
+art_illumina --noALN -ss HS25 -i 'label_s__Methanobrevibacter_B sp900314605.fa' -p -o 'art_s__Methanobrevibacter_B sp900314605.' -l 150 -f 0.11100674421474477 -m 200 -s 10 -rs 1
+art_illumina --noALN -ss HS25 -i 'label_s__Methanobrevibacter_B boviskoreani.fa' -p -o 'art_s__Methanobrevibacter_B boviskoreani.' -l 150 -f 0.2707153897818837 -m 200 -s 10 -rs 1
