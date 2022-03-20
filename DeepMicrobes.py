dea1b3f60917a8c3807cac546923ba11be57918f
Wanxin Li <w328li@uwaterloo.ca>
2022-03-20 12:06:00 +0800

    debug

--- DeepMicrobes.py
+++ DeepMicrobes.py
@@ -149,7 +149,7 @@ def model_fn(features, labels, mode, params):
 
     # Added for calculating the training accuracy of the entire dataset
    
-    entire_features, entire_labels = entire_input(flags.FLAGS.input_tfrec)
+    entire_features, entire_labels = entire_input(flags.FLAGS.input_tfrec, flags.FLAGS.cpus)
     entire_logits = model(entire_features)
 
     entire_predictions = {
