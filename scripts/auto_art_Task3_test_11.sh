ad8450ed7952376d9f345579b432340b61e7f033
Wanxin Li <w328li@uwaterloo.ca>
2022-03-22 01:40:17 -0400

    updated GTDB_mini related files

---
+++ scripts/auto_art_Task3_test_11.sh
@@ -0,0 +1,5 @@
+art_illumina --noALN -ss HS25 -i 'RUG120.fasta' -p -o 'art_RUG120.' -l 150 -f 1.1648472399915355 -m 200 -s 10 -rs 11
+art_illumina --noALN -ss HS25 -i 'RUG121.fasta' -p -o 'art_RUG121.' -l 150 -f 1.2569393526762334 -m 200 -s 10 -rs 11
+art_illumina --noALN -ss HS25 -i 'RUG012.fasta' -p -o 'art_RUG012.' -l 150 -f 1.2677511573511608 -m 200 -s 10 -rs 11
+art_illumina --noALN -ss HS25 -i 'RUG076.fasta' -p -o 'art_RUG076.' -l 150 -f 1.6101166851561732 -m 200 -s 10 -rs 11
+art_illumina --noALN -ss HS25 -i 'RUG011.fasta' -p -o 'art_RUG011.' -l 150 -f 1.2076677239126663 -m 200 -s 10 -rs 11
