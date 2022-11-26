1752068d27d003d09b20f2d62ba7ada0c495751f
Wanxin Li <w328li@uwaterloo.ca>
2022-11-26 00:11:31 -0800

    added first_1000.sh to extract the first 1000 reads

---
+++ scripts/first_1000.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+## get the first 1,000 reads 
+
+task=$1
+dir=""
+if [[ $task -eq 2 ]]; then
+    dir="/mnt/sda/DeepMicrobes-data/rumen_mags_reads_Task2_small_all"
+fi
+
+new_dir=${dir}_kraken
+mkdir $new_dir
+
+if [[ $task -eq 2 ]]; then
+    for file in ${dir}/*1_trimmed.fa ; do
+        cat ${file} |head -2000 > ${new_dir}/file
+    done
+fi
